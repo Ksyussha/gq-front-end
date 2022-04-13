ebeee46f4e7c003128896f7408d4106c25eb6b2d
ITstart <itstart@itstep.loc>
2022-04-13 18:26:49 +0600

    first commit

---
+++ apollo.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  client: {
+    service: {
+      name: "my-app",
+      url: "http://38.242.229.113:8080/v1/graphql",
+    },
+    includes: [
+      "./src/**/*.js",
+      "./src/**/*.vue",
+    ],
+  }
+};
